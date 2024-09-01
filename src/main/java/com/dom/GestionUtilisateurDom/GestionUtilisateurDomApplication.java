bbbcf414a41aad78c23821f14f80f6df574e4dd2
Abdoulaye Aliou Sall <gallebaba71@gmail.com>
2024-09-01 00:20:29 +0000

    initial commit

---
+++ src/main/java/com/dom/GestionUtilisateurDom/GestionUtilisateurDomApplication.java
@@ -0,0 +1,13 @@
+package com.dom.GestionUtilisateurDom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUtilisateurDomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUtilisateurDomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dom/GestionUtilisateurDom/GestionUtilisateurDomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dom.GestionUtilisateurDom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUtilisateurDomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
